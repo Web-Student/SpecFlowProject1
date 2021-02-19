b8483f24fb16d91061153653c6a5461fdbea84af
Rachel Allen <rachel.allen1@students.snow.edu>
2021-02-18 22:47:22 -0700

    Add project files.

---
+++ SpecFlowProject1/Features/Calculator.feature
@@ -0,0 +1,34 @@
+﻿Feature: Calculator
+![Calculator](https://specflow.org/wp-content/uploads/2020/09/calculator.png)
+Simple calculator for adding **two** numbers
+
+Link to a feature: [Calculator](SpecFlowProject1/Features/Calculator.feature)
+***Further read***: **[Learn more about how to generate Living Documentation](https://docs.specflow.org/projects/specflow-livingdoc/en/latest/LivingDocGenerator/Generating-Documentation.html)**
+
+@mytag
+Scenario: Add two numbers
+	Given the first number is 50
+	And the second number is 70
+	When the two numbers are added
+	Then the result should be 120
+
+@2nd Test
+Scenario: Add one positive and 1 negative
+	Given the first number is 50
+	And the second number is -30
+	When the two numbers are added
+	Then the result should be 20
+
+@subtraction
+Scenario:  Subtract a smaller number from a larger number
+	Given the first number is 88
+	And the second number is 32
+	When the two numbers are subtracted
+	Then the result should be 56
+
+@sub2
+Scenario: Subtract a larger number from a smaller number
+	Given the first number is 18
+	And the second number is 32
+	When the two numbers are subtracted
+	Then the result should be -14
---
+++ SpecFlowProject1/Steps/CalculatorStepDefinitions.cs
@@ -0,0 +1,73 @@
+﻿using FluentAssertions;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowProject1.Steps
+{
+    [Binding]
+    public sealed class CalculatorStepDefinitions
+    {
+
+        // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
+
+        private readonly ScenarioContext _scenarioContext;
+
+        public CalculatorStepDefinitions(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+
+        [Given("the first number is (.*)")]
+        public void GivenTheFirstNumberIs(int number)
+        {
+            //TODO: implement arrange (precondition) logic
+            // For storing and retrieving scenario-specific data see https://go.specflow.org/doc-sharingdata
+            // To use the multiline text or the table argument of the scenario,
+            // additional string/Table parameters can be defined on the step definition
+            // method. 
+
+            _scenarioContext.Add("num1", number);
+
+        }
+
+        [Given("the second number is (.*)")]
+        public void GivenTheSecondNumberIs(int number)
+        {
+            //TODO: implement arrange (precondition) logic
+            // For storing and retrieving scenario-specific data see https://go.specflow.org/doc-sharingdata
+            // To use the multiline text or the table argument of the scenario,
+            // additional string/Table parameters can be defined on the step definition
+            // method. 
+
+            _scenarioContext.Add("num2", number);
+        }
+
+        [When("the two numbers are added")]
+        public void WhenTheTwoNumbersAreAdded()
+        {
+            //TODO: implement act (action) logic
+
+            int n1 = _scenarioContext.Get<int>("num1");
+            int n2 = _scenarioContext.Get<int>("num2");
+            _scenarioContext.Add("sum", n1 + n2);
+
+        }
+
+        [Then("the result should be (.*)")]
+        public void ThenTheResultShouldBe(int result)
+        {
+            //TODO: implement assert (verification) logic
+
+            int ans = _scenarioContext.Get<int>("sum");
+            ans.Should().Be(result);
+        }
+
+        [When(@"the two numbers are subtracted")]
+        public void WhenTheTwoNumbersAreSubtracted()
+        {
+            int n1 = _scenarioContext.Get<int>("num1");
+            int n2 = _scenarioContext.Get<int>("num2");
+            _scenarioContext.Add("sum", n1 - n2);
+        }
+
+    }
+}
